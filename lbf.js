216437ce6c6d1fafe6ca942d02c28c495d6bf08e
Shaunak Bhandarkar <shaunak.bhandarkar@gmail.com>
2014-10-24 18:38:32 -0700

    YYYEEEESSSS IT FINALLY WORKED!
    I finally found out about the z-index property. It solved ALL my
    problems!

--- lbf.js
+++
@@ -1,9 +0,0 @@
-function switchColor(linkId)
-  {
-    document.getElementById(linkId + "link").style.color="#000000";
-  }
-  
-function switchColorBack(linkId)
-  {
-    document.getElementById(linkId + "link").style.color="#ffffff";
-  }
